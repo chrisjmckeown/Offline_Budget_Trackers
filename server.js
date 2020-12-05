3ea0390106cbcc2f53ef3949554713b1121f660e
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-12-05 14:04:32 +1100

    mongoDB Atlas db connected and routes changed to awaits.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
 const PORT = 8080;
 
@@ -15,14 +16,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log("App running on: http://localhost:" + PORT);
-});
+// Connect to Mongo
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("MongoDB Connected...");
+
+    app.listen(PORT, () => {
+      console.log("App running on: http://localhost:" + PORT);
+    });
+  })
+  .catch((err) => console.log(err));
